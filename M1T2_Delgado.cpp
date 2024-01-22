e32c8ffacd88ac4451f8623acc871d37cb8a3e15
delgador8307 <157392654+delgador8307@users.noreply.github.com>
2024-01-22 16:28:26 +0000

    Finished M1T2

---
+++ M1T2_Delgado.cpp
@@ -0,0 +1,27 @@
+// CSC 134
+// M1T2
+// Rowan Delgado
+// 1/22/2024
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Introduce Yourself
+    cout << "Hello World!" << endl;
+    cout << "My Name is Rowan" << "\n";
+
+    //ask a question
+    cout << "What's Your Name?\n";
+
+    // Get Their Responce
+    char name[25];
+    cin.get(name,25);
+
+
+    //Respond
+    cout << "Nice to meet you, " << name << endl << endl;
+
+
+    return 0;
+}

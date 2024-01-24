ce63b47d927da8effe2189320302a17a8d834c97
delgador8307 <157392654+delgador8307@users.noreply.github.com>
2024-01-24 16:19:47 +0000

    Started M1HW

---
+++ M1HW_Delgado.cpp
@@ -0,0 +1,27 @@
+/*
+CSC 134
+M1HW - Movie
+Rowan Delgado
+1/24/2024
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Talk about Movies
+
+    //Set up Variables
+    string movie_name = "Boondock Saints";
+    int release_year = 1999;
+    double box_office_gross = 30.4;
+    double rental_sales = 50.3;
+
+    cout << "the movie";
+
+
+
+
+
+
+}

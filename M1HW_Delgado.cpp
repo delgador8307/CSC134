ce4d0a390e4d150b9b55902cb03bdf72b052e1f2
delgador8307 <157392654+delgador8307@users.noreply.github.com>
2024-01-24 16:31:08 +0000

    Finished M1HW

--- M1HW_Delgado.cpp
+++ M1HW_Delgado.cpp
@@ -16,11 +16,16 @@ int main() {
     int release_year = 1999;
     double box_office_gross = 30.4;
     double rental_sales = 50.3;
+    int budget = 6;
 
-    cout << "the movie";
+    cout << "the movie " << movie_name << " came out in " << release_year << endl;
+    cout << "the movie made a whopping " << box_office_gross << " thousand dollas at the box office\n";
+    cout << "though the movie was a bit of a flop in theaters it did end up making a profit\n";
+    cout << "It ending up bringing in " << rental_sales << " million dollars in rental sales from the late Blockbuster\n";
+    cout << "This well exceeded the budget of " << budget << " million dollars and helped to cement it as a cult classic\n\n";
 
 
-
+    return 0;
 
 
 

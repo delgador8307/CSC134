12e8292d94b4cbef5b2c292e26fc76148d2f9323
delgador8307 <157392654+delgador8307@users.noreply.github.com>
2024-01-29 16:16:11 +0000

    M2T1 - Receipt provisionally done

--- M2/M2T1_Delgado.cpp
+++ M2/M2T1_Delgado.cpp
@@ -6,13 +6,43 @@ Rowan Delgado
 */
 
 #include <iostream>
+#include <string>
+#include <iomanip>
 using namespace std;
 
 int main() {
+    //Set up Variables
+    double meal_price = 0;
+    double tax_percent = 0;
+    double tax_amount = 0;
+    double tip = 0;
+    double subtotal = 0;
+    double total = 0;
+    cout << setprecision(2) << fixed;
+
+    //Declare Variables
+    meal_price = 5.99;
+    tax_percent = .08;
+
+    //calculate tax and subtotal
+    tax_amount = (tax_percent*meal_price);
+    subtotal = (meal_price+tax_amount);
+
+    //TODO: Make it line up and fix decimals
+
+    cout << "Taco\t\t:\t$" << meal_price << endl;
+    cout << "Tax\t\t:\t$" << tax_amount << endl;
+    cout << "Subtotal\t:\t$" << subtotal << endl << endl;
+    cout << "Tip\t\t:\t$";
+    cin >> tip;
+
+    //Calculate total
+    total = (subtotal+tip);
+
+    //Print Total
+    cout << "\n\nTotal\t\t:\t$" << total << endl << endl;
 
 
-
-
-    return 0
+    return 0;
 }
 
